7aa4c242722672b5b590295891ec16ee216d7e67
Valya Vyrodova <valya.vyrodova@gmail.com>
2021-05-27 13:51:34 +0300

    learn variables

--- js-lessons/app.js
+++ js-lessons/app.js
@@ -1,1 +1,53 @@
-alert ('1')
+// 1 Variables 
+
+//var name = 'Valentyna'
+const firstName = 'Valentyna'
+//const lastName = 'Vyrodova' // string
+//const age = 26 // number
+const isProgrammer = true // boolean
+//name = 'Valya'
+//age = false
+//console.log(firstName)
+//console.log(age)
+//console.log(isProgrammer)
+const _ = 'private'
+const $ = 'some value'
+// const if = 'mkef' // err
+const withNum5 = '5'
+// const 5withNum = 5 // err
+
+// 2 Mutation
+//console.log('Person name: ' + firstName + ', person age: ' + age)
+//console.log(age.toString())
+//alert('Person name: ' + firstName + ', person age: ' + age)
+//prompt('Please enter your last name')
+//const lastname = prompt ('Please enter your last name')
+//alert (firstName + ' ' + lastname)
+
+
+// Operators
+let currentYear = 2021
+const birthYear = 1994
+
+//const age = currentYear - birthYear
+//console.log (age)
+const a = 10
+const b = 5
+let c = 32
+//c = c + a
+// c += a
+// c -= a
+// c *= a
+// c /=a
+
+// console.log(a + b)
+// console.log(a-b)
+// console.log(a*b)
+// console.log(a/b)
+// //console.log(currentYear++)
+// //console.log(currentYear)
+// console.log(++currentYear)
+// console.log(--currentYear)
+// console.log(c)
+
+// 4 Data types
