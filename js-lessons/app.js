062a91dcc10273d8a450ed0db277becc1b0b4d2b
Valya Vyrodova <valya.vyrodova@gmail.com>
2021-05-28 18:38:54 +0300

    lesson04-Numbers

--- js-lessons/app.js
+++ js-lessons/app.js
@@ -1,171 +1,54 @@
-// 1 Variables 
+// const num = 42 // integer
+// const float = 42.42 // float
+// console.log(float)
+// const pow = 10e3
+// console.log(pow)
+// console.log('MAX_SAFE_INTEGER', Number.MAX_SAFE_INTEGER)
+// console.log('Math.pow 53', Math.pow(2, 53) - 1)
+// console.log('MIN_SAFE_INTEGER', Number.MIN_SAFE_INTEGER)
+// console.log('MAX_VALUE', Number.MAX_VALUE)
+// console.log('MIN_VALUE', Number.MIN_VALUE)
+// console.log('MAX_VALUE', Number.POSITIVE_INFINITY)
+// console.log('MIN_VALUE', Number.NEGATIVE_INFINITY)
+// console.log('1/0', 1 / 0)
+// console.log(Number.NaN)
+// console.log(typeof NaN)
+// const weird = 2 / undefined
+// console.log(isNaN(weird))
+// console.log(Number.isNaN(42))
+// console.log(Number.isFinite(Infinity))
+// console.log(Number.isFinite(42))
 
-//var name = 'Valentyna'
-// const firstName = 'Valentyna'
-//const lastName = 'Vyrodova' // string
-//const age = 26 // number
-// const isProgrammer = true // boolean
-//name = 'Valya'
-//age = false
-//console.log(firstName)
-//console.log(age)
-//console.log(isProgrammer)
-// const _ = 'private'
-// const $ = 'some value'
-// const if = 'mkef' // err
-// const withNum5 = '5'
-// const 5withNum = 5 // err
+// const stringInt = '40'
+// const stringFloat = '40.40'
+// console.log(stringInt + 2) // 402
+// console.log(Number.parseInt(stringInt) + 2)
+// console.log(Number(stringInt) + 2)
+// console.log(+stringInt + 2)
 
-// 2 Mutation
-//console.log('Person name: ' + firstName + ', person age: ' + age)
-//console.log(age.toString())
-//alert('Person name: ' + firstName + ', person age: ' + age)
-//prompt('Please enter your last name')
-//const lastname = prompt ('Please enter your last name')
-//alert (firstName + ' ' + lastname)
+// console.log(parseInt(stringFloat) + 2) //42
+// console.log(parseFloat(stringFloat) + 2)
+// console.log(+stringFloat + 2)
 
+// console.log(0.4 + 0.2) // 0.60000001
+// console.log((2/5) + (1/5)) //0.60000001
+// console.log((0.4 + 0.2).toFixed(4)) // string
+// console.log((0.4 + 0.2).toFixed(1)) // string
+// console.log(+(0.4 + 0.2).toFixed(1))
+// console.log(parseFloat((0.4 + 0.2).toFixed(1)))
 
-// Operators
+//BigInt
 
-// let currentYear = 2021
-// const birthYear = 1994
+// console.log(Number.MAX_SAFE_INTEGER)
+// console.log(typeof 900719925474099199999)
+// console.log(typeof 900719925474099199999n) //bigInt
+// console.log(900719925474099199999n - 900719925474099199n)
+// console.log(-900719925474099199999n)
+// console.log(-900719925474099199999n.585) // error
 
-//const age = currentYear - birthYear
-//console.log (age)
-// const a = 10
-// const b = 5
-// let c = 32
-//c = c + a
-// c += a
-// c -= a
-// c *= a
-// c /=a
+//console.log(10n - 4) // error
+// console.log(parseInt(10n) - 4)
+// console.log(10n - BigInt(4))
+// console.log(5n / 2n) //2n
 
-// console.log(a + b)
-// console.log(a-b)
-// console.log(a*b)
-// console.log(a/b)
-// //console.log(currentYear++)
-// //console.log(currentYear)
-// console.log(++currentYear)
-// console.log(--currentYear)
-// console.log(c)
-
-// 4 Data types
-
-// const isProgrammer = true // boolean
-// const name = 'Valentyna'
-// const age = 26
-// let x
-// console.log(typeof isProgrammer)
-// console.log(typeof name)
-// console.log(typeof age)
-// console.log(typeof x)
-// console.log(typeof null)
-
-// 5. Operator precedence
-// const fullAge = 26
-// const birthYear = 1994
-// const currentYear = 2021
-
-// // > < >= <=
-// const isFullAge = currentYear - birthYear >= fullAge // 26 >= 27
-// console.log(isFullAge)
-
-// 6. Conditional statement 
-// const courseStatus = 'pending' // ready, fail, pending
-// if (courseStatus === 'ready') {
-//     console.log('Курс уже готов и его можно проходить')
-// } else if (courseStatus === 'pending') {
-//     console.log( 'Курс пока находится в процессе разработки')
-// } else {
-//     console.log('Курс не получился')
-// }
-
-// const num1 = 42 // number
-// const num2 = '42' // string
-
-// console.log(num1 === num2)
-
-// const isReady = false
-
-// if (isReady) {  // isReady === true
-//     console.log('Все готово')
-// } else {
-//     console.log('Все не готово')
-// }
-
-//Тернарное выражение
-// isReady ? console.log ('Все готово') : console.log('Все не готово')
-
-
-// 7. Булевая логика
-
-// // 8. Functions 
-// function calculateAge(year) {
-//    return 2020 - year
-// }
-// //const myAge = calculateAge(1994)
-// //console.log(myAge)
-// //console.log(calculateAge(1994))
-
-// function logInfoAbout(name, year) {
-//     const age = calculateAge(year)
-
-//     if (age > 0) {
-//         console.log('Человек по имени ' + name + ' сейчас имеет возраст ' + age)    
-//     } else {
-//         console.log('Вообще-то это уже будущее')
-//     }
-// }
-// logInfoAbout('Валентина', 1994)
-// logInfoAbout('Kostya', 1996)
-// logInfoAbout('Женя', 2023)
-
-// 9. Arrays
-//const cars = ['Mersedess', 'Ford', 'Mazda']
-//console.log(cars)
-//const cars = new Array('Mersedess', 'Ford', 'Mazda')
-//console.log(cars)
-// console.log(cars[0])
-// console.log(cars[1])
-// console.log(cars[2])
-// console.log(cars.length)
-
-// cars[0] = 'Porsche'
-// cars[3] = 'Mersedes'
-// cars[cars.length] = 'Zaporozhez'
-// console.log(cars)
-
-// 10. Loop
-// const cars = ['Mersedess', 'Ford', 'Mazda']
-// for (let i = 0; i < cars.length; i++) {
-//     const car = cars[i]
-//     console.log(car)
-// }
-
-// const cars = ['Mersedess', 'Ford', 'Mazda']
-// for (let car of cars) {
-// console.log(car)
-
-// }
-
-// 11. Objects
-const person = {
-    firstName: 'Valentyna',
-    lastName: 'Vyrodova',
-    Year: '1994',
-    languages: ['Ukraine', 'Russian', 'English'],
-    hasWife: false,  
-    greet: function () {
-        console.log('greet from person')
-    }
-}
-console.log(person.firstName)
-console.log(person['lastName'])
-const key = 'Year'
-console.log(person[key])
-person.hasWife = true
-person.isProgrammer = true
-console.log(person)
-person.greet()
+// Math
