1af6494bbaed860c0fc26be5489a63c49a5e3b45
Valya Vyrodova <valya.vyrodova@gmail.com>
2021-05-31 14:02:34 +0300

    conditional

---
+++ js-tasks/js-codecademy/conditional.js
@@ -0,0 +1,68 @@
+// 1.
+
+let sale = true;
+sale = false
+if (sale) {
+    console.log('Time to buy!')
+}
+
+// 2. 
+
+let sale = true;
+sale = false;
+
+if (sale) {
+    console.log('Time to buy!');
+} else {
+    console.log('Time to wait for a sale.')
+}
+
+
+// 3. 
+
+let hungerLevel = 7
+if (hungerLevel < 7) {
+    console.log('Time to eat!')
+} else {
+    console.log('We can eat later!')
+}
+
+// 4.
+let mood = 'sleepy';
+let tirednessLevel = 6;
+if (mood === 'sleepy' && tirednessLevel > 8) {
+    console.log('time to sleep')
+} else {
+    console.log('not bed time yet')
+}
+
+// 5.
+
+let wordCount = 10;
+
+if (wordCount) {
+  console.log("Great! You've started your work!");
+} else {
+  console.log('Better get to work!');
+}
+
+
+let favoritePhrase = '';
+
+if (favoritePhrase) {
+  console.log("This string doesn't seem to be empty.");
+} else {
+  console.log('This string is definitely empty.');
+}
+
+// 6.
+
+let tool = 'marker';
+
+// Use short circuit evaluation to assign  writingUtensil variable below:
+let writingUtensil = tool || 'pen';
+
+console.log(`The ${writingUtensil} is mightier than the sword.`);
+
+// 7.
+
