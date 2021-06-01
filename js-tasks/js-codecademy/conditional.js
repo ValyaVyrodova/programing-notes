69a90eae9078d8b11cf4868cbde7a67359762cbb
Valya Vyrodova <valya.vyrodova@gmail.com>
2021-06-01 22:33:12 +0300

    SWITCH

--- js-tasks/js-codecademy/conditional.js
+++ js-tasks/js-codecademy/conditional.js
@@ -66,3 +66,48 @@ console.log(`The ${writingUtensil} is mightier than the sword.`);
 
 // 7.
 
+let isLocked = false;
+isLocked ? console.log('You will need a key to open the door.') : console.log('You will not need a key to open the door.');
+
+let isCorrect = true;
+isCorrect ? console.log('Correct!') : console.log('Incorrect!');
+
+let favoritePhrase = 'Love That!';
+favoritePhrase === 'Love That!'? console.log('I love that!') : console.log("I don't love that!");
+
+// 8.
+
+let season = 'summer';
+
+if (season === 'spring') {
+  console.log('It\'s spring! The trees are budding!');
+} else if (season === 'winter') {
+  console.log('It\'s winter! Everything is covered in snow.')
+} else if (season === 'fall') {
+  console.log('It\'s fall! Leaves are falling!')
+} else if (season === 'summer') {
+  console.log('It\'s sunny and warm because it\'s summer!')
+}
+else {
+  console.log('Invalid season.');
+}
+
+
+// 9.
+
+let athleteFinalPosition = 'first place';
+
+switch (athleteFinalPosition) {
+  case 'first place' :
+  console.log('You get the gold medal!')
+  break
+  case 'second place' :
+  console.log('You get the silver medal!')
+  break
+  case 'third place' :
+  console.log('You get the bronze medal!')
+  break
+  default :
+  console.log('No medal awarded.')
+  break
+}
