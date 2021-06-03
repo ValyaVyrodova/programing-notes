bf1d60352a2bec871f02b2f608f818824ba40b07
Valya Vyrodova <valya.vyrodova@gmail.com>
2021-06-03 12:11:21 +0300

    IntroductionTask

---
+++ js-tasks/js-codecademy/introduction.js
@@ -0,0 +1,25 @@
+// 1. Kelvin Weather
+
+const kelvin = 0 // forecast in Kelvin
+let celsius = kelvin - 273 // forecast in Celsius
+let fahrenheit = celsius * (9 / 5) + 32 // calculate the Fahrenheit
+Math.floor(fahrenheit) // korrect the result 
+console.log(`The temperature is ${fahrenheit} degrees Fahrenheit.`)
+let newton = celsius * (33 / 100)
+Math.floor(newton)
+console.log(`The temperature is ${newton} degrees Newton.`)
+
+
+
+// 2. Dogs years
+const myAge = 22
+let earlyYears = 2
+earlyYears *= 10.5
+let laterYears = myAge - 2
+laterYears *= 4
+console.log(earlyYears)
+console.log(laterYears)
+let myAgeInDogYears = earlyYears + laterYears
+let myName = 'Valya'.toLowerCase()
+console.log(myName)
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.`)
